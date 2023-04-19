b6a1f1e67153319760468cc6d01ffab772e62a7f
VavasseurKevin <minhluan.phan@gmail.com>
2023-04-19 15:46:02 +0200

    Connect to API : login page  and create an acccount User in the Profile page

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -73,13 +73,13 @@ const Register = () => {
 
     if (checkInputs()) {
       await axios
-        .post(import.meta.env.API_URL + "api/users", {
+        .post(import.meta.env.VITE_API_URL + "api/users", {
           email: email,
           firstname: firstName,
           lastname: lastName,
           password: password,
           newsletter: newsLetter,
-          roles: ["ROLES_USER"],
+          roles: ["ROLE_USER"],
         })
         .then(() => {
           navigate("/login");
@@ -98,9 +98,8 @@ const Register = () => {
           Register
         </h1>
 
-        {errors["password"] &&
-          Object.values(errors["password"]).map((error, index) => <p key={index}>{error}</p>)}
-
+{(errors["password"] && typeof errors["password"]!=="string") ?
+          Object.values(errors["password"]).map((error, index) => <p key={index}>{error}</p>) : ""}
         <form
           style={{ fontFamily: "ClashDisplay-Medium" }}
           className="mt-6 text-xl"
